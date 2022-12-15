7b1cdd2d7df7337d0c17589998d3e1f274af0b2e
Binderiya <binderiyatsogthers@gmail.com>
2022-12-15 16:08:46 +0800

    finished

--- Binderiya/script.js
+++ Binderiya/script.js
@@ -1,1 +1,15 @@
-const a = 10;
+
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  // printing star
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
